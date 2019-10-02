ec4e2c122b484f155cf8c4d41c0bc4c03edf81a9
Matthew-Yorke <matthewyorke867@gmail.com>
2019-10-01 22:08:27 -0400

    Initial Window Class
    This update is to add the initial creation of a display window. The basic setup of the Bebop namespace is used to create a Window class under the Bebop::Graphics namespace that will allow a user the create a window with their desired specifications. The main function added a quick test of this window creation.

---
+++ Bebop/Source/BebopCore/Graphics/Window.cpp
@@ -0,0 +1,134 @@
+//*********************************************************************************************************************
+//
+// File: Window.cpp
+//
+// Description:
+//    This class is sued to create  a display window using the Allegroe 5 library.
+//
+//*********************************************************************************************************************
+
+#include "Window.h"
+
+namespace Bebop { namespace Graphics
+{
+//*********************************************************************************************************************
+// Public Methods - Start
+//*********************************************************************************************************************
+
+   //******************************************************************************************************************
+   //
+   // Method: Window
+   //
+   // Description:
+   //    Constructor for the window class that takes in arguments to specify the window dimensions and then calls to
+   //    intialize the creation of the window.
+   //
+   // Arguments:
+   //    aWidth  - The width of the display window.
+   //    aHeight - The height of the display window.
+   //
+   // Return:
+   //    N/A
+   //
+   //******************************************************************************************************************
+   Window::Window(int aWidth, int aHeight) : mpWindow(nullptr), mWidth(aWidth), mHeight(aHeight)
+   {
+      if (false == Initialize())
+      {
+         Terminate();
+      }
+   }
+   
+   //******************************************************************************************************************
+   //
+   // Method: ~Window
+   //
+   // Description:
+   //    Destructor of the window class that takes steps to free any allocated memory from this class.
+   //
+   // Arguments:
+   //    N/A
+   //
+   // Return:
+   //    N/A
+   //
+   //******************************************************************************************************************
+   Window::~Window()
+   {
+      Terminate();
+   }
+   
+//*********************************************************************************************************************
+// Public Methods - End
+//*********************************************************************************************************************
+
+//*********************************************************************************************************************
+// Protected Methods - Start
+//*********************************************************************************************************************
+
+   // There are currently no protected methods for this class.
+
+//*********************************************************************************************************************
+// Protected Methods - End
+//*********************************************************************************************************************
+
+//*********************************************************************************************************************
+// Private Methods - Start
+//*********************************************************************************************************************
+
+   //******************************************************************************************************************
+   //
+   // Method: Initialize
+   //
+   // Description:
+   //    Calls allegro functionality to create the display window with the current dimensions described by the user.
+   //
+   // Arguments:
+   //    N/A
+   //
+   // Return:
+   //    True  - The initialization of the window was successful.
+   //    False - The initializaiton of the window was unsuccessful.
+   //
+   //******************************************************************************************************************
+   bool Window::Initialize()
+   {
+      // Initialize allegro and catch if a failure occurs.
+      // TODO: This should be moved to a different class for the game enginee initialization.
+      if (!al_init())
+      {
+         return false;
+      }
+   
+      if (!(mpWindow = al_create_display(mWidth, mHeight)))
+      {
+         return false;
+      }
+   
+      return true;
+   }
+
+   //******************************************************************************************************************
+   //
+   // Method: Terminate
+   //
+   // Description:
+   //    Termination the window. This can be used to destroy the window with or without destroying the entire class
+   //    object.
+   //
+   // Arguments:
+   //    N/A
+   //
+   // Return:
+   //    N/A
+   //
+   //******************************************************************************************************************
+   void Window::Terminate()
+   {
+      al_destroy_display(mpWindow);
+   }
+
+//*********************************************************************************************************************
+// Private Methods - End
+//*********************************************************************************************************************
+}}
--- Bebop/Source/Main.cpp
+++ Bebop/Source/Main.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+#include "BebopCore/Graphics/Window.h"
+
+using namespace Bebop::Graphics;
+
 int main()
 {
+   Window test(400, 400);
+
+   int a;
+   std::cin >> a;
    return 0;
 }
